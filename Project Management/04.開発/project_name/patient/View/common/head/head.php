e3ef413b2cae85fe8cd42326f710ca2c147f13b8
PhooPwintMaung <phoopwintmaung92@gmail.com>
2022-10-05 09:39:34 +0630

    header folder name change to head header Done!!!!!

---
+++ Project Management/04.開発/project_name/patient/View/common/head/head.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <!-- CSS File -->
+    <link rel="stylesheet" href="../../resources/css/root.css">
+    <link rel="stylesheet" href="./head.css">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Source+Sans+Pro&display=swap" rel="stylesheet" />
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg border-bottom border-dark" >
+        <div class="container-fluid ">
+            <a class="navbar-brand" href="#">Logo</a>
+            <div class="collapse navbar-collapse d-flex flex-row-reverse " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item me-5 text-white">
+                        <a class="nav-link " aria-current="page" href="#">Hospital Locations</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link  " aria-current="page" href="#">Find Doctors</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link  " aria-current="page" href="#">Booking</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link  " aria-current="page" href="#">Blogs</a>
+                    </li>
+                    <li class="nav-item me-3 ">
+                        <div class="profile">
+                            <img src="./profile.jpg" alt="" class="profile_img">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</body>
+
+</html>
