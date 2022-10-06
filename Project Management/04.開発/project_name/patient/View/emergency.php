8aa635d48054ad7afef12068c1f1abe09885d8e9
PhooPwintMaung <phoopwintmaung92@gmail.com>
2022-10-06 11:14:47 +0630

    commit

--- Project Management/04.開発/project_name/patient/View/emergency.php
+++ Project Management/04.開発/project_name/patient/View/emergency.php
@@ -14,27 +14,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
+
 <body>
+    <!-- Header -->
     <?php
     include("./common/head.php")
     ?>
     <!-- Search Box -->
     <div id="first_aid_search" class="me-4">
-    <div class="searchbox mt-2" >
-      <span class="search_text fw-semibold">Search</span>
-      <input type="text" class="search_input ps-5" placeholder="" />
-      <i class="fa-solid fa-magnifying-glass search_icon"></i>
-    </div>
+        <div class="searchbox mt-2">
+            <span class="search_text fw-semibold">Search</span>
+            <input type="text" class="search_input ps-5" placeholder="" />
+            <i class="fa-solid fa-magnifying-glass search_icon"></i>
+        </div>
     </div>
     <!-- First Tab -->
     <div class="important_tab text-center mt-5">
         <h3 class="text-uppercase important_tab_header">why is it important to know first aid?</h3>
         <div class="important_tab_para">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas maxime facere laboriosam id, quae sequi error veritatis culpa doloremque eveniet ipsum unde quibusdam sapiente, perspiciatis quisquam ipsa consectetur repudiandae quia?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas maxime facere laboriosam id, quae sequi error veritatis culpa doloremque eveniet ipsum unde quibusdam sapiente, perspiciatis quisquam ipsa consectetur repudiandae quia?</p>
         </div>
         <hr class="line" />
     </div>
-    
+
     <!-- Cards -->
     <div class="first_aid_card my-4">
         <h1 class="first_aid_card_header mb-3">First Aid For Heart Attack</h1>
@@ -129,6 +131,7 @@
         </div>
         <hr class="line" />
     </div>
+    <!-- Footer -->
     <?php
     include("./common/footer.php")
     ?>
