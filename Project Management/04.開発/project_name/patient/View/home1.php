a8513be858267d589c38507c73eb8795d45ed7ab
PhooPwintMaung <phoopwintmaung92@gmail.com>
2022-10-07 16:02:50 +0630

    home1 slider add

--- Project Management/04.開発/project_name/patient/View/home1.php
+++ Project Management/04.開発/project_name/patient/View/home1.php
@@ -6,43 +6,61 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
-  <!-- Root Css -->
-  <link rel="stylesheet" href="./resources/css/root.css">
-  <!-- Css link -->
-  <link rel="stylesheet" href="./resources/css/home1.css?v=" time()>
-  <!-- cdn link -->
+  <!-- Splide.js cdn link -->
   <link rel="stylesheet" href="./resources/js/splide-4.1.3/dist/css//splide.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/css/themes/splide-skyblue.min.css">
-
+  <!-- JS file for Slider -->
+  <script src="./resources/js/home1.js" defer></script>
+  <!-- Css -->
+  <link rel="stylesheet" href="./resources/css/root.css">
+  <link rel="stylesheet" href="./resources/css/home1.css?v=" time()>
+  
+</head>
 
 <body>
   <!-- Header -->
   <?php
   include("./common/head.php")
   ?>
-  <section class="splide" aria-label="Splide Basic HTML Example">
-    <div class="splide__track">
+   <!-- Medicine Slider -->
+  <section class="splide medicine my-5" aria-label="Splide Basic HTML Example">
+    <div class="splide__track medicine_wrapper">
       <ul class="splide__list">
-        <li class="splide__slide">Slide 01</li>
-        <li class="splide__slide">Slide 02</li>
-        <li class="splide__slide">Slide 03</li>
+        <li class="splide__slide">
+          <div class="slide_img_div">
+            <img src="./storages/image/emergency.jpg" alt="" class="slide_img" />
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="slide_img_div">
+            <img src="./storages/image/emergency.jpg" alt="" class="slide_img" />
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="slide_img_div">
+            <img src="./storages/image/emergency.jpg" alt="" class="slide_img" />
+          </div>
+        </li>
       </ul>
     </div>
   </section>
+  <!-- First Aid For Emergency Situations Tab-->
+  <div class="blog_tab_wrapper">
+    <div class="blog_tab mb-4">
+      <div class="blog_tab_text_div">
+        <h3 class="blog_tab_text">First Aid For Emergency Situations</h3>
+      </div>
+      <div class="blog_tab_img_div">
+        <img src="./storages/image/emergency1.png" alt="" class="blog_tab_img" />
+      </div>
+    </div>
+    <hr class="line" />
+  </div> 
+  <!-- Footer -->
   <?php
   include("./common/footer.php")
   ?>
   <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js"></script>
-  <script>
-    var splide = new Splide('.splide', {
-      type: 'loop',
-      perPage: 3,
-      autoplay: true,
-    });
-
-    splide.mount();
-  </script>
-
 </body>
 
 </html>
