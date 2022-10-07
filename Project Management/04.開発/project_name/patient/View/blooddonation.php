1624022a48cc37687ed9e284af9191a864d25d5e
htawlon <minhtawlon111@gmail.com>
2022-10-07 19:23:58 +0630

    css file miss

--- Project Management/04.開発/project_name/patient/View/blooddonation.php
+++ Project Management/04.開発/project_name/patient/View/blooddonation.php
@@ -6,20 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blood Donation</title>
-    <!-- Css -->
-    <link rel="stylesheet" href="./resources/css/blooddonation.css?v="time()>
-    <!-- Css Root  -->
-    <link rel="stylesheet" href="./resources/css/root.css">
     <!-- Boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
+    <!-- Css -->
+    <link rel="stylesheet" href="./resources/css/blooddonation.css?v="time()>
+    <link rel="stylesheet" href="./resources/css/root.css">
+    
+    <!-- Css Root  -->
+    
 </head>
 
 <body>
     <!-- Header -->
     <?php
-    include("./common/head.php")
+    include("./common/head.php");
     ?>
     <!-- Blood Donation Header -->
     <h3 class="blood_donate_header my-5">Blood Donation Center</h3>
--- Project Management/04.開発/project_name/patient/View/doctor.php
+++ Project Management/04.開発/project_name/patient/View/doctor.php
@@ -69,6 +69,7 @@
                         </label>
                     </label>
                 </div>
+                <hr>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                     <label class="form-check-label" for="flexRadioDefault1">
@@ -107,6 +108,7 @@
                         </label>
                     </label>
                 </div>
+                <hr>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                     <label class="form-check-label" for="flexRadioDefault1">
@@ -144,6 +146,7 @@
                         </label>
                     </label>
                 </div>
+                <hr>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                     <label class="form-check-label" for="flexRadioDefault1">
