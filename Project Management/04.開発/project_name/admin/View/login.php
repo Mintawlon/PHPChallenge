602625ee49540a636b1be21023213635f0694025
LAPTOP-BQVNBT3L\ASUS <kaungzanthaw27904@gmail.com>
2022-10-07 19:33:39 +0630

    last commit

--- Project Management/04.開発/project_name/admin/View/login.php
+++ Project Management/04.開発/project_name/admin/View/login.php
@@ -10,6 +10,11 @@
     <link rel="stylesheet" href="./resources/css/root.css">
     <!-- jquery -->
     <script src="./resources/js/jquery3.6.0.js"></script>
+    <!-- Css -->
+    <link rel="stylesheet" href="./resources/css/login.css">   
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -17,7 +22,7 @@
 
     <!-- Main Content -->
     <div class="container">
-
+        <p class="text-center align-items-center">Admin Log In</p>
     </div>
 
 
