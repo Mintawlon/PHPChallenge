60e562b1a8d05e3f4a0fb5347e22d6daae1e6e26
htawlon <minhtawlon111@gmail.com>
2022-10-07 21:38:36 +0630

    home page edit

--- Project Management/04.開発/project_name/patient/View/home1.php
+++ Project Management/04.開発/project_name/patient/View/home1.php
@@ -44,6 +44,155 @@
       </ul>
     </div>
   </section>
+  <!--scroll card-->
+  <div class="container">
+        <ul class="cards">
+            <li class="card">
+                <div>
+                    img
+                    <h3 class="card-title">Service 1</h3>
+                    <div class="card-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 2</h3>
+                    <div class="card-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus
+                            fuga et repellat in maiores.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 3</h3>
+                    <div class="card-content">
+                        <p>Phasellus ultrices lorem vel bibendum ultricies. In hendrerit nulla a ante dapibus pulvinar
+                            eu eget quam.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 4</h3>
+                    <div class="card-content">
+                        <p>Aenean posuere mauris quam, pellentesque auctor mi bibendum nec. Sed scelerisque lacus nisi,
+                            quis auctor lorem ornare vel.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 5</h3>
+                    <div class="card-content">
+                        <p>Vestibulum pharetra fringilla felis sit amet tempor. Interdum et malesuada fames ac ante
+                            ipsum primis in faucibus. Cras et arcu sit amet est consequat feugiat. Nam ut sapien
+                            pulvinar.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 6</h3>
+                    <div class="card-content">
+                        <p>Donec ut tincidunt nisl. Vivamus eget eros id elit feugiat mollis. Nam sed sem quis libero
+                            finibus tempor.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 7</h3>
+                    <div class="card-content">
+                        <p>Aliquam eget nisl auctor, sollicitudin ipsum at, dignissim ligula. Donec tincidunt in elit et
+                            pellentesque. Integer posuere metus ac massa mollis euismod.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 8</h3>
+                    <div class="card-content">
+                        <p> Vivamus eget eros id elit feugiat mollis. Nam sed sem quis libero finibus tempor.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 9</h3>
+                    <div class="card-content">
+                        <p>Duis id congue turpis. Donec sodales porta felis, nec ultricies ante. Nam placerat vitae
+                            metus sit amet tempor. Aliquam ac dictum est.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 10</h3>
+                    <div class="card-content">
+                        <p>Pellentesque eget eros eget justo efficitur fermentum.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                    <h3 class="card-title">Service 11</h3>
+                    <div class="card-content">
+                        <p>Phasellus posuere nec nibh ut tincidunt. Aenean mollis turpis non eros posuere, at luctus leo
+                            hendrerit. Integer non libero sapien.</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+            <li class="card">
+                <div>
+                  <img src="./storages/image/vaccine.png" style="">
+                    <h3 class="card-title">Service 12</h3>
+                    <div class="card-content">
+                        <p>Vestibulum ante ipsum primis in</p>
+                    </div>
+                </div>
+                <div class="card-link-wrapper">
+                    <a href="" class="card-link">Learn More</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+  <!--scorll card-->
   <!-- First Aid For Emergency Situations Tab-->
   <div class="blog_tab_wrapper">
     <div class="blog_tab mb-4">
@@ -54,7 +203,7 @@
         <img src="./storages/image/emergency1.png" alt="" class="blog_tab_img" />
       </div>
     </div>
-    <hr class="line" />
+    <hr class="line"/>
   </div> 
   <!-- Footer -->
   <?php
