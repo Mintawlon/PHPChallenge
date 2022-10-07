4ddf99f150803a1e07c8cfad4b7ffd6d259220d0
LAPTOP-BQVNBT3L\ASUS <kaungzanthaw27904@gmail.com>
2022-10-07 10:44:06 +0630

    login

--- Project Management/04.開発/project_name/patient/View/forgotpsw.php
+++ Project Management/04.開発/project_name/patient/View/forgotpsw.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login to your account</title>
+    <title>Sign Up</title>
     <!-- Css -->
-    <link rel="stylesheet" href="./resources/css/login.css">
+    <link rel="stylesheet" href="./resources/css/forgotpsw.css">
     <!-- Css Root  -->
     <link rel="stylesheet" href="./resources/css/root.css">
     <!-- Boostrap -->
@@ -18,28 +18,18 @@
 <body>
     <div class="conainer">
         <!-- Logo  -->
-        <img class="mt-5 ms-5" src="" alt="LOGO">
+        <img class=" mt-5 ms-5" src="" alt="LOGO">
         <!-- body -->
-        <div class="body">
-            <p class="wclback">Welcome back</p>
-            <p class="login mt-5 text-white">Login to your account</p>
+        <div class="body ">
+            <br><br><br><br>
+            <p class="pasw mt-5 fw-semibold">Forgot Your Password?</p>
             <!-- input box -->
-            <div class="inpbox ms-5 mt-1">
+            <div class="inpbox ms-5 mt-5">                
+                <input type="text" class="name form-control" placeholder="Email Address">               
                 <br>
-                <input type="text" class="name form-control" placeholder="Login with email">
-                <br>
-                <br>
-                <br>
-                <input type="text" class="name form-control" placeholder="Password">
-                
-                
-                <a href=""><p class="forgotpsw  text-decoration-underline">Forgot Password?</p></a>
                 <button type="submit" class="btnsignup mt-1 form-control">
-                    Login In
+                    Send Email
                 </button>
-                <a href="">
-                    <p class="mt-2 reg text-white fs-6 text-decoration-underline d-flex">Not Registered yet?</p>
-                </a>
             </div>
             <div>
                 <img class="login_img ms-5" src="../View/components/card/image/image_login.png.png" alt="">
