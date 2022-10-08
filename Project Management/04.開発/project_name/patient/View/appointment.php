1fc12fe6ae59c9f444ef3f23b112186f5491c48e
DESKTOP-7S8BJH3\Noe Noe <kmms94an@gmail.com>
2022-10-08 20:28:12 +0630

    commit

--- Project Management/04.開発/project_name/patient/View/appointment.php
+++ Project Management/04.開発/project_name/patient/View/appointment.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
+    <title>Appointment</title>
     <!-- Boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
@@ -30,7 +30,7 @@
             <div class="row">
                 <div class="col-md-6 col-sm-12">
                     <!-- Appointment form -->
-                    <form class="form_align">
+                    <form>
 
                         <div class="input_align mb-3">
 
@@ -73,9 +73,7 @@
 
                         <!-- Button -->
                         <div class="btn_align">
-                            <button type="submit" class="btn btn-lg btn_color text_wrap">
-                                <div class="btn_textSize"> Book</div>
-                            </button>
+                            <button type="button" class="btn btn-lg btn_color">Book an Appointment</button>
                         </div>
                     </form>
                 </div>
