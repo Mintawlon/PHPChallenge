8e33cb396d88174cefaefbc8881da340292eec58
PhooPwintMaung <phoopwintmaung92@gmail.com>
2022-10-08 21:57:48 +0630

    Carousel Image Responsive Done!!!!!!

--- Project Management/04.開発/project_name/patient/View/home1.php
+++ Project Management/04.開発/project_name/patient/View/home1.php
@@ -2,71 +2,57 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
-  <!-- Splide.js cdn link -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/css/themes/splide-skyblue.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js"></script>
-  <!-- JS file for Slider -->
-  
-  <!-- Css -->
-  <link rel="stylesheet" href="./resources/css/root.css">
-<<<<<<< HEAD
-  <link rel="stylesheet" href="./resources/css/home1.css?v="<?php echo time() ?>>
-=======
-  <!-- <link rel="stylesheet" href="./resources/css/home1.css?v="<?=time()?>> -->
-  <link rel="stylesheet" href="./resources/css/home1.css">
->>>>>>> dec7341ebf534c9ddefdca6786fb384f6be2d855
-  
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- Splide.js cdn link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/css/themes/splide-skyblue.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js"></script>
+    <!-- JS file for Slider -->
+
+    <!-- Css -->
+    <link rel="stylesheet" href="./resources/css/root.css">
+    <link rel="stylesheet" href="./resources/css/home1.css">
 </head>
 
 <body>
-  <!-- Header -->
-  <?php
-  include("./common/head.php")
-  ?>
-   <!-- Medicine Slider -->
-  <section class="splide medicine my-5" id="carousel" aria-label="Splide Basic HTML Example">
-    <div class="splide__track medicine_wrapper">
-      <ul class="splide__list">
-        <li class="splide__slide">
-          <div class="slide_img_div">
-            <img src="./storages/image/emergency1.png" alt="" class="slide_img" />
-          </div>
-        </li>
-        <li class="splide__slide">
-          <div class="slide_img_div">
-            <img src="./storages/image/blog_tab_img1.png" alt="" class="slide_img" />
-          </div>
-        </li>
-      </ul>
+    <!-- Header -->
+    <?php
+    include("./common/head.php")
+    ?>
+    <!-- Medicine Slider -->
+    <div class="carousel_slider">
+        <section class="splide first_slide my-5" id="carousel" aria-label="Splide Basic HTML Example">
+            <div class="splide__track first_slide_wrapper">
+                <ul class="splide__list">
+                    <li class="splide__slide">
+                        <div class="first_slide_div">
+                            <img src="./storages/image/blood_donate.png" alt="" class="first_slide_img" />
+                        </div>
+                    </li>
+                    <li class="splide__slide">
+                        <div class="first_slide_div">
+                            <img src="./storages/image/Covid (2).png" alt="" class="first_slide_img" />
+                        </div>
+                    </li>
+                    <li class="splide__slide">
+                        <div class="first_slide_div">
+                            <img src="./storages/image/Dengue Fever.png" alt="" class="first_slide_img" />
+                        </div>
+                    </li>
+                    <li class="splide__slide">
+                        <div class="first_slide_div">
+                            <img src="./storages/image/hospital ads.png" alt="" class="first_slide_img" />
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     </div>
-  </section>
-  <section class="splide medicine my-5" id="medicine" aria-label="Splide Basic HTML Example">
-    <div class="splide__track medicine_wrapper">
-      <ul class="splide__list">
-        <li class="splide__slide">
-          <div class="slide_img_div">
-            <img src="./storages/image/emergency.jpg" alt="" class="slide_img" />
-          </div>
-        </li>
-        <li class="splide__slide">
-          <div class="slide_img_div">
-            <img src="./storages/image/emergency.jpg" alt="" class="slide_img" />
-          </div>
-        </li>
-        <li class="splide__slide">
-          <div class="slide_img_div">
-            <img src="./storages/image/emergency.jpg" alt="" class="slide_img" />
-          </div>
-        </li>
-      </ul>
     </div>
-  </section>
-  <!--scroll card-->
-  <div class="container">
+    <!--scroll card-->
+    <div class="container">
         <ul class="cards">
             <li class="card">
                 <div>
@@ -201,7 +187,7 @@
             </li>
             <li class="card">
                 <div>
-                  <img src="./storages/image/vaccine.png" >
+                    <img src="./storages/image/vaccine.png">
                     <h3 class="card-title">Service 12</h3>
                     <div class="card-content">
                         <p>Vestibulum ante ipsum primis in</p>
@@ -213,24 +199,24 @@
             </li>
         </ul>
     </div>
-  <!--scorll card-->
-  <!-- First Aid For Emergency Situations Tab-->
-  <div class="blog_tab_wrapper">
-    <div class="blog_tab mb-4">
-      <div class="blog_tab_text_div">
-        <h3 class="blog_tab_text">First Aid For Emergency Situations</h3>
-      </div>
-      <div class="blog_tab_img_div">
-        <img src="./storages/image/emergency1.png" alt="" class="blog_tab_img" />
-      </div>
+    <!--scorll card-->
+    <!-- First Aid For Emergency Situations Tab-->
+    <div class="blog_tab_wrapper">
+        <div class="blog_tab mb-4">
+            <div class="blog_tab_text_div">
+                <h3 class="blog_tab_text">First Aid For Emergency Situations</h3>
+            </div>
+            <div class="blog_tab_img_div">
+                <img src="./storages/image/emergency1.png" alt="" class="blog_tab_img" />
+            </div>
+        </div>
+        <hr class="line" />
     </div>
-    <hr class="line"/>
-  </div> 
-  <!-- Footer -->
-  <?php
-  include("./common/footer.php")
-  ?>
-  <script src="./resources/js/home1.js"></script>
+    <!-- Footer -->
+    <?php
+    include("./common/footer.php")
+    ?>
+    <script src="./resources/js/home1.js"></script>
 </body>
 
 </html>
