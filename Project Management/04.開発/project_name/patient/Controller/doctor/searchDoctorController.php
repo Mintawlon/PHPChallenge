54c50e40afd5dc5468ad3e18d19dda94fbb91004
PhooPwintMaung <phoopwintmaung92@gmail.com>
2022-10-18 11:56:09 +0630

    Doctor Search

--- Project Management/04.開発/project_name/patient/Controller/doctor/searchDoctorController.php
+++ Project Management/04.開発/project_name/patient/Controller/doctor/searchDoctorController.php
@@ -1,25 +1,43 @@
 <?php
 include("../../Model/dbConnection.php");
-if(isset($_POST["health"])){
-    $search = "health";
+// if(isset($_POST["health"])){
+//     $search = "health";
     
-    $sql = $pdo->prepare("SELECT * FROM doctor WHERE speciality LIKE :search");
-    $sql->bindValue(":search","%".$search."%");
+//     $sql = $pdo->prepare("SELECT * FROM doctor WHERE speciality LIKE :search");
+//     $sql->bindValue(":search","%".$search."%");
+//     $sql->execute();
+//     $searchList = $sql->fetchAll(PDO::FETCH_ASSOC);
+//     echo "<pre>";
+//     var_export($searchList) ;
+    
+//     // echo json_encode($searchList);
+// }
+// if(isset($_POST["dentist"])){
+//     $search = "dentistry";
+    
+//     $sql = $pdo->prepare("SELECT * FROM doctor WHERE speciality LIKE :search");
+//     $sql->bindValue(":search","%".$search."%");
+//     $sql->execute();
+//     $searchList = $sql->fetchAll(PDO::FETCH_ASSOC);
+//     echo "<pre>";
+//     var_export($searchList) ;
+// }
+if(isset($_POST["searchText"])){
+    $search = $_POST["searchText"];
+    
+    $sql = $pdo->prepare("SELECT * FROM doctor WHERE speciality=:search");
+    $sql->bindValue(":search",$search);
     $sql->execute();
     $searchList = $sql->fetchAll(PDO::FETCH_ASSOC);
-    echo "<pre>";
-    var_export($searchList) ;
     
-    // echo json_encode($searchList);
+    echo json_encode($searchList);
 }
-if(isset($_POST["dentist"])){
-    $search = "dentistry";
-    
-    $sql = $pdo->prepare("SELECT * FROM doctor WHERE speciality LIKE :search");
-    $sql->bindValue(":search","%".$search."%");
-    $sql->execute();
-    $searchList = $sql->fetchAll(PDO::FETCH_ASSOC);
-    echo "<pre>";
-    var_export($searchList) ;
-}
+//     $search = "General Health";
+// $sql = $pdo->prepare("SELECT * FROM doctor WHERE speciality=:search");
+// $sql->bindValue(":search",$search);
+// $sql->execute();
+// $searchList = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+// echo "<pre>";
+// var_export($searchList);
 ?>
--- Project Management/04.開発/project_name/patient/View/doctor.php
+++ Project Management/04.開発/project_name/patient/View/doctor.php
@@ -14,6 +14,8 @@ include "../Controller/doctor/doctorInfoController.php";
     <link rel="stylesheet" href="./resources/css/doctor.css?v=" time()>
     <!-- Css Root  -->
     <link rel="stylesheet" href="./resources/css/root.css?v=" time()>
+    <script src="./resources/js/jquery3.6.0.js"></script>
+    <script src="./resources/js/doctor.js" defer></script>
 </head>
 
 <body>
@@ -27,29 +29,23 @@ include "../Controller/doctor/doctorInfoController.php";
         <div class="row">
             <div class="col-12 mt-5 btn-row">
                 <!-- Blue Square Btn -->
-                <form action="../Controller/doctor/searchDoctorController.php" method="POST">
-                    <input type="hidden" name="general health">
-                <button class="square_special_btn mobile" name="health"><i class="fa-solid fa-stethoscope"></i> &nbsp; &nbsp; General Health</button>
-                </form>
-                <button class="square_special_btn"><i class="fa-solid fa-lungs"></i> &nbsp; &nbsp;Nephology</button>
-                <button class="square_special_btn"><i class="fa-solid fa-lungs"></i> &nbsp; &nbsp;Orthopaedics</button>
-                <button class="square_special_btn"><i class="fa-solid fa-eye"></i> &nbsp; &nbsp; Ophthalmology</button>
+                <button class="square_special_btn mobile" id="generalHealth"><i class="fa-solid fa-stethoscope me-3"></i>General Health</button>
+                <button class="square_special_btn" id="pulmonology"><i class="fa-solid fa-lungs me-3"></i>Pulmonology </button>
+                <button class="square_special_btn" id="paediatric"><i class="fa-solid fa-baby me-3"></i>Paediatric </button>
+                <button class="square_special_btn" id="ophthalmology"><i class="fa-solid fa-eye me-3"></i>Ophthalmology</button>
             </div>
         </div>
         <div class="row">
             <div class="col-12 mt-5 btn-row">
                 <!-- Blue Square Btn -->
-                <button class="square_special_btn mobile"><i class="fa-solid fa-brain"></i> &nbsp; &nbsp;Neurology</button>
-                <button class="square_special_btn"><i class="fa-solid fa-person-pregnant"></i> &nbsp; &nbsp; Pregancy</button>
-                
-                <form action="../Controller/doctor/searchDoctorController.php" method="POST">
-                    <input type="hidden" name="dentist">
-                    <button class="square_special_btn"><i class="fa-solid fa-tooth"></i>&nbsp; &nbsp; General Dentistry</button>
+                <button class="square_special_btn mobile" id="neurology"><i class="fa-solid fa-brain me-3"></i>Neurology</button>
+                <button class="square_special_btn" id="og"><i class="fa-solid fa-person-pregnant me-3"></i>OB-GYN</button>
+                <button class="square_special_btn" id="dentist"><i class="fa-solid fa-tooth me-3"></i>Dentist</button>
                 </form>
             </div>
         </div>
 
-        <div class="row mt-5">
+        <div class="row mt-5" id="doctorSearch">
             <!--card-->
             <?php foreach ($doctorInfo as $doctor) { ?>
                 <div class="col-sm-12 col-md-6 col-lg-4 text-center">
@@ -57,7 +53,7 @@ include "../Controller/doctor/doctorInfoController.php";
                         <dvi class="image">
                             <img src="./storages/image/<?= $doctor["profile_photo"] ?>" class="card-img-top" alt="...">
                             <h5 class="mt-2"><?= $doctor["doctor_name"] ?></h5>
-                            <p><?= $doctor["speciality"] ?></p>
+                            <p ><?= $doctor["speciality"] ?></p>
                         </dvi>
                         <div class="card-body">
                             <div class="contact">
