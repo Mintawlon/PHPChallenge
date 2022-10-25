63a9125a53b081856e4219dc817cf6c1ff928333
LAPTOP-BQVNBT3L\ASUS <kaungzanthaw27904@gmail.com>
2022-10-25 22:45:19 +0630

    medicine img

--- Project Management/04.開発/project_name/patient/View/home1.php
+++ Project Management/04.開発/project_name/patient/View/home1.php
@@ -115,7 +115,7 @@ if (!isset($_SESSION["email"])) {
                 <li class="card">
                     <div>
                         <div class="vaccine_div">
-                            <img src="../../admin/View/storages/medicineImage/<?= $medicine["medicine_image"] ?>" class="vaccine_img">
+                            <img src="../../admin/View/storages/medicineImage/<?= $medicine["medicine_image"] ?>" class="vaccine_img text-center align-items-center ">
                         </div>
                         <h3 class="card-title"><?=$medicine["medicine_name"] ?></h3>
                         <div class="card-content">
