104fdc29a6d67eac9e7e568636154b532e31c5d2
LAPTOP-BQVNBT3L\ASUS <kaungzanthaw27904@gmail.com>
2022-10-26 17:12:02 +0630

    commit

--- Project Management/04.開発/project_name/patient/Controller/forgotPasswordController.php
+++ Project Management/04.開発/project_name/patient/Controller/forgotPasswordController.php
@@ -6,22 +6,22 @@ include "./common/commonFunction.php";
 include "./common/mailSender.php";
 session_start();
 
-if(isset($_POST["resetPsw"])){
+if (isset($_POST["resetPsw"])) {
     $userEmail = $_POST["email"];
-    
+
     $sql = $pdo->prepare("SELECT * FROM user_register WHERE email_address = :email");
     $sql->bindValue(":email", $userEmail);
     $sql->execute();
     $userInfo = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-    
 
-    if(count($userInfo) !=0){
+
+    if (count($userInfo) != 0) {
 
         // new password
         $newPassword = getRamdom(10);
 
-        $sql= $pdo->prepare(
+        $sql = $pdo->prepare(
             "UPDATE user_register SET
                 password = :password,
                 update_date = :updateDate
@@ -29,24 +29,30 @@ if(isset($_POST["resetPsw"])){
             "
         );
         $sql->bindValue(":updateDate", date("Y/m/d"));
-        $sql->bindValue(":password",  
-        password_hash($newPassword,PASSWORD_DEFAULT));
+        $sql->bindValue(
+            ":password",
+            password_hash($newPassword, PASSWORD_DEFAULT)
+        );
         $sql->bindValue(":id", $userInfo[0]['id']);
         $sql->execute();
 
-         //mail send
+        //mail send
         $mail = new SendMail();
         $mail->sendMail(
             $userInfo[0]['email_address'],
-            "Reset Password Complete",
-            "<h1>Password Reset Complete</h1>
-            <p>New Password : $newPassword</p>"
+            "Password Reset Complete",
+            "<h1>Hello!!<h1>
+            <h3><b>We received a request to reset your password and we'll give a new paassword.</b></h3>
+            <h4><b>Here a new password : $newPassword</b></h4>
+            <h4>Thank You for joining with us!!</h4>
+            <h4>Vr Carer Team :<a> vrcarer1010@gmail.com <a/></h4>"
         );
-         header("Location: ../View/login.php");
-    }else{
+        header("Location: ../View/login.php");
+    } else {
         $_SESSION["forgotEmailWrong"] = $userEmail;
         header("Location: ../View/forgotpsw.php");
     }
 }
 ?>
+
 
